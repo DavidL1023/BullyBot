206df6e3b89a9682028b857899d7c1d94235aec2
David L <davidlacava66@gmail.com>
2021-08-26 03:14:29 -0400

    added bunch of functionality
    added commands, server messages

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Discord Bot
+I learned a lot from making this bot and working with the Discord API. Also found out about Nodemon, pretty cool package. First time working with JavaScript, this is by far the most complex language I've learned so far, or maybe it's just because I was working with an API and the code wasn't simple, idk. I can see why JavaScript is so popular, I'll be making my own website soon so good thing I learned it now. 
 
-Cool thing is nodemon
-have the bot have a command where you can set the loser of the server, and whenever they type it tells them how stupid they are with random responses
+Invite link for bot => https://discord.com/api/oauth2/authorize?client_id=879598602882785300&permissions=8&scope=bot%20applications.commands
 
+(Warning, the bot is offensive)
 # Dependencies
-/ DiscordJS / Dotenv /
-
+/ [DiscordJS](https://www.npmjs.com/package/discord.js) / [Dotenv](https://www.npmjs.com/package/dotenv) /
 
 ## How To Use
 NOTE: For authorization when using the bot, you must create '.env' file in parent directory and enter this line of code
 --> DISCORDJS_BOT_TOKEN=(*insert bots token from Discord*)
 
 ## Video in Action
+
+
+
+
+
+have the bot have a command where you can set the loser of the server, and whenever they type (maybe random chance) it tells them how stupid they are with random responses
+
+says you look like this then send image (have file of images)
+
+remind people of how much a piece of shit they are
+you know no ones listening, right?
+
+you'll regret adding me on join
+
+bold all responses
--- src/index.js
+++ src/index.js
@@ -1,35 +1,67 @@
-// Javascript imports
-const Discord = require("discord.js");
+console.clear();
+// Require the necessary discord.js classes
 require('dotenv').config();
-// Create required objects for Discord
-const intents = new Discord.Intents(32767);
-const client = new Discord.Client({ intents });
+const { Client, Intents } = require('discord.js');
+// Create a new client instance
+const client = new Client({ intents: 32767 });
 
-// Bot events
-let prefix = "!";
 
-client.on('ready', () => {
+// --------------- BOT EVENTS -----------------
+client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (message) => {
-    //Check if message starts with command prefix, if not it returns
+const prefix = '%';
+
+//Join server message
+client.on('guildCreate', async (guild) => {
+    console.log(`[Server]: Joined \"${guild.name}\"!`);
+    await client.users.cache.get(guild.ownerId).send({
+        content: 'You\'ll regret adding me.',
+        files: [{
+          attachment: 'images/trollge.jpg',
+          name: 'trollge.jpg'
+        }]
+      }).catch(console.error);
+    console.log('[Bot]: Sent join message to server owner :D')
+});
+
+//Leave server message
+client.on('guildDelete', (guild) => {
+    console.log(`[Server]: Left \"${guild.name}\" :(`);
+});
+
+//Check if message starts with command prefix, if not it returns
+client.on('messageCreate', async (message) => {
     if(!message.content.startsWith(prefix)){
         return;
+    }else{
+        console.log(`[${message.author.tag}]: ${message.content}`);
     }
 
-    //Creates array of commands after prefix
+    //Creates array of words after prefix
     const args = message.content.substring(prefix.length).split(/ +/);
 
+    //Commands
+    let reply;
     switch(args[0]){
-        case "hello":
-            message.reply("yo");
+        case 'help':
+        case 'Help':
+            reply = `Here's what commands I have: \n ${prefix}Hello \n ${prefix}Say`;
+            await message.reply(reply).catch(console.error);
             break;
-        case "say":
-            message.reply(args.slice(1).join(" "));
+        case 'hello':
+        case 'Hello':
+            reply = `yo`;
+            await message.reply(reply).catch(console.error);
+            break;
+        case 'say':
+        case 'Say':
+            reply = args.slice(1).join(' ');
+            await message.reply(reply).catch(console.error);
             break;
     }
-
+    console.log(`[Bot]: ${reply}\n`);
 });
 
 
