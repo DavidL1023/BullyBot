42b7cae8f656f4b799c6683b15ca2fea55fe9361
David L <davidlacava66@gmail.com>
2021-08-28 02:01:24 -0400

    added more commands, added percent chance, added insults

--- README.md
+++ README.md
@@ -1,27 +1,21 @@
 # Discord Bot
 I learned a lot from making this bot and working with the Discord API. Also found out about Nodemon, pretty cool package. First time working with JavaScript, this is by far the most complex language I've learned so far, or maybe it's just because I was working with an API and the code wasn't simple, idk. I can see why JavaScript is so popular, I'll be making my own website soon so good thing I learned it now. 
 
-Invite link for v12 bot => https://discord.com/api/oauth2/authorize?client_id=879598602882785300&permissions=8&scope=bot%20applications.commands
+Invite link for v13 bot => https://discord.com/api/oauth2/authorize?client_id=879598602882785300&permissions=0&scope=bot%20applications.commands
 
 (Warning, the bot is offensive)
 # Dependencies
-/ [DiscordJS](https://www.npmjs.com/package/discord.js) / [Dotenv](https://www.npmjs.com/package/dotenv) /
+/ [DiscordJS](https://www.npmjs.com/package/discord.js) / [Insult](https://www.npmjs.com/package/insult) / [Dotenv](https://www.npmjs.com/package/dotenv) /
 
 ## How To Use
-NOTE: For authorization when using the bot, you must create '.env' file in parent directory and enter this line of code
+
+
+**BOT INFO**
+For authorization when using the bot, you must create '.env' file in parent directory and enter this line of code
 --> DISCORDJS_BOT_TOKEN=(*insert bots token from Discord*)
+
+Also, you can modify the Insult package by entering ./node_modules/insult/data/insults.js, add insults here. 
 
 ## Video in Action
 
-
-
-
-
-have the bot have a command where you can set the loser of the server, and whenever they type (maybe random chance) it tells them how stupid they are with random responses
-
-says you look like this then send image (have file of images)
-
-remind people of how much a piece of shit they are
-you know no ones listening, right?
-
-find api for insults or smth
+figure out how to have multiple instances of bots for servers
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.clear();
 const config = require('./Data/config.json');
-// Require the necessary discord.js classes
+// Require the necessary modules
+const insulter = require('insult');
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 // Create a new client instance
@@ -21,31 +22,32 @@ function getUserFromMention(mention) {
 
 // --------------- BOT EVENTS -----------------
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`[Client] Logged in as ${client.user.tag}!\n`);
     client.user.setActivity('you cry.', { 
         type: 'WATCHING' 
     });
 });
 
 var bullyTarget;
+var lastInsultGenerated;
 
 // Join server message
 client.on('guildCreate', async (guild) => {
-    console.log(`[Server]: Joined \"${guild.name}\"!`);
+    console.log(`[Client]: Joined \"${guild.name}\"!\n`);
     await client.users.cache.get(guild.ownerId).send({
         content: 'You\'ll ***regret*** adding me.',
         files: [{
-          attachment: 'img/trollge.jpg',
+          attachment: 'src/Data/trollge.jpg',
           name: 'trollge.jpg'
         }]
       })
-      .then(console.log('[Bot]: Sent join message to server owner :D'))
+      .then(console.log('[Client]: Sent join message to server owner :D\n'))
       .catch(console.error);
 });
 
 // Leave server message
 client.on('guildDelete', (guild) => {
-    console.log(`[Server]: Left \"${guild.name}\" :(`);
+    console.log(`[Client]: Left \"${guild.name}\" :(\n`);
 });
 
 // Check if message starts with command prefix, if not it returns
@@ -64,44 +66,104 @@ client.on('messageCreate', async (message) => {
     let botReply;
     let mentionedUser;
 
-    switch(args[0].toLowerCase()){
-        case 'help':
-            botReply = `Here's what I can do: \n ${config.prefix}Bully <@user> \n ${config.prefix}Current \n ${config.prefix}Clear`;
-            break;
-        case 'bully':
-            mentionedUser = getUserFromMention(args[1]);
-            if(typeof mentionedUser !== 'undefined'){
-                if(mentionedUser.tag == client.user.tag){
-                    botReply = 'I\'m not going to bully myself.';
+    if(message.author !== bullyTarget){
+        switch(args[0].toLowerCase()){
+            case 'help':
+                botReply = `**Once you set my target, I'll have a ~35% chance to make fun of them when they type**\nHere's what I can do:\n\t${config.prefix}Bully *<@user>*\n\t${config.prefix}Current\n\t${config.prefix}Clear`;
+                break;
+            case 'bully':
+                mentionedUser = getUserFromMention(args[1]);
+                if(typeof mentionedUser !== 'undefined'){
+                    if(mentionedUser == client.user){
+                        botReply = 'I\'m not going to bully myself.';
+                    }else if(mentionedUser == bullyTarget) {
+                        botReply = 'I\'m already doing that.';
+                    }else if(typeof bullyTarget !== 'undefined'){
+                        bullyTarget = mentionedUser;
+                        botReply = `Switching target to ${bullyTarget}.`
+                    }else{
+                        bullyTarget = mentionedUser;
+                        botReply = `Now bullying ${bullyTarget}.`
+                    }
                 }else{
-                    bullyTarget = mentionedUser;
-                    botReply = `Now bullying ${bullyTarget.username}.`
+                    botReply = 'What kind of idiot does it take to not be able to use a command properly?'
                 }
-            }else{
-                botReply = 'What kind of idiot does it take to not be able to use a command properly?'
-            }
-            break;
-        case 'current':
-            if(typeof bullyTarget !== 'undefined'){
-                botReply = `Currently shitting on ${bullyTarget}.`
-            }else{
-                botReply = 'No target yet.'
-            }
-            break;
-        case 'clear':
-            bullyTarget = undefined;
-            botReply = 'No longer bullying.'
-            break;
-        default:
-            botReply = 'You messed up the command ya clown.'
+                break;
+            case 'current':
+                if(typeof bullyTarget !== 'undefined'){
+                    botReply = `Currently shitting on ${bullyTarget}.`
+                }else{
+                    botReply = 'I\'m currently not bullying anyone.'
+                }
+                break;
+            case 'clear':
+                if(typeof bullyTarget !== 'undefined'){
+                    botReply = `No longer bullying ${bullyTarget}.`
+                    bullyTarget = undefined;
+                }else{
+                    botReply = 'I\'m currently not bullying anyone.'
+                }
+                break;
+            default:
+                botReply = 'You messed up the command ya clown.'
+        }
+    // If target is typing command
+    }else{
+        switch(args[0].toLowerCase()){
+            case 'help':
+                botReply = 'Help yourself shitwad.';
+                break;
+            case 'bully':
+                mentionedUser = getUserFromMention(args[1]);
+                if(typeof mentionedUser !== 'undefined'){
+                    if(mentionedUser == client.user){
+                        botReply = 'I\'m not going to bully myself.';
+                    }else if(mentionedUser == bullyTarget) {
+                        botReply = 'I\'m already doing that.';
+                    }else{
+                        botReply = 'Yeah uh, that\'s not how it works buddy. You\'re the target.'
+                    }
+                }else{
+                    botReply = 'You can\'t even use the command correctly, sad.'
+                }
+                break;
+            case 'current':
+                botReply = `Currently shitting on your dumb ass.`
+                break;
+            case 'clear':
+                botReply = 'Ask someone a little more intelligent than yourself to let you go.'
+                break;
+            default:
+                botReply = 'You messed up the command, but I didn\'t expect any better from you.'
+        }
     }
     await message.reply(botReply)
-    .then(console.log(`[Bot]: ${botReply}\n`))
-    .catch(console.error);
+        .then(console.log(`[${client.user.tag}]: ${botReply}\n`))
+        .catch(console.error);
 });
 
 // Check if bully target typed
+client.on('messageCreate', async (message) => {
+    if(message.content.startsWith(config.prefix)){
+        return;
+    }else if(message.author == bullyTarget){
+        console.log(`*[${message.author.tag}]: ${message.content}`);
 
+        // Send insult on percent chance and ensure not repeating itself
+        let roll = Math.random()
+        if(roll > 0.65){
+            let botReply = insulter.Insult();
+            while(botReply == lastInsultGenerated){
+                botReply = insulter.Insult();
+            }
+            await message.reply(botReply)
+        .then(console.log(`[${client.user.tag}]: ${botReply}\n`))
+        .catch(console.error);
+        }else{
+            console.log('[Client]: Did not roll insult chance.\n');
+        }
+    }
+});
 
 // Authorize access to bot
 client.login(process.env.DISCORDJS_BOT_TOKEN);
